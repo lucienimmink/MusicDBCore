3d9fb6feded8122ceab94db686432e45467896ae
Lucien Immink <lucien@arielext.org>
2016-06-06 22:15:16 +0200

    [TASK] Update core to parse items

--- dist/core.js
+++ dist/core.js
@@ -10,10 +10,20 @@
     var musicdbcore = (function () {
         function musicdbcore() {
             this.VERSION = "1.0.0";
-            console.log('Core init', VERSION);
+            console.log("Core init " + this.VERSION);
         }
         musicdbcore.prototype.setSourceJson = function (json) {
             console.log("this json has " + json.length + " records");
+            for (var _i = 0, json_1 = json; _i < json_1.length; _i++) {
+                var line = json_1[_i];
+                var artist = new MusicDBObject.Artist(line);
+                var album = new MusicDBObject.Album(line);
+                var track = new MusicDBObject.Track(line);
+                album.artist = artist;
+                track.artist = artist;
+                track.album = album;
+                console.log(track.url());
+            }
         };
         return musicdbcore;
     }());
--- src/org/arielext/musicdb/core.ts
+++ src/org/arielext/musicdb/core.ts
@@ -1,9 +1,19 @@
 export class musicdbcore {
     private const VERSION:string = "1.0.0"; 
     constructor () {
-        console.log('Core init', VERSION);
+        console.log(`Core init ${this.VERSION}`);
     }
     setSourceJson(json:any) {
         console.log(`this json has ${json.length} records`);
+        for (let line of json) {
+            let artist = new MusicDBObject.Artist(line);
+            let album = new MusicDBObject.Album(line);
+            let track = new MusicDBObject.Track(line);
+
+            album.artist = artist;
+            track.artist = artist;
+            track.album = album;
+            console.log(track.url());
+        }
     }
 }
