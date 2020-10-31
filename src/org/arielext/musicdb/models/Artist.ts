13231456e4b0e482085cc867e7e902707ab1b3da
Lucien Immink <lucien@arielext.org>
2020-10-31 15:34:44 +0100

    encode

--- src/org/arielext/musicdb/models/Artist.ts
+++ src/org/arielext/musicdb/models/Artist.ts
@@ -18,7 +18,7 @@ export default class Artist {
       this.albumArtist = json.albumartist || json.albumArtist || '';
       // tslint:disable-next-line:max-line-length
       this.sortName = this.stripFromName((this.albumArtist) ? this.albumArtist.toUpperCase() : (json.sortName) ? json.sortName.toUpperCase() : this.name.toUpperCase(), ['the ', '"', 'a ']);
-      this.sortName = btoa(this.sortName);
+      this.sortName = btoa(encodeURIComponent(this.sortName))
       this.bio = json.bio;
       this.isCollection = (this.albumArtist) ? this.name !== this.albumArtist : false;
     }
